477457a10f7852c47638c14e6385fae9781b3fc3
Chip Lee <chiplee@gmail.com>
2023-12-11 15:25:50 +0400

    retrofit to code server

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+pip install -e transformers-4.28.1
+pip install datasets
+pip install accelerate
+pip install openai
+
+mkdir -p dataset
+mkdir -p output_dir
+
+
+# 检查test_tfqa_mc.json文件是否存在，如果不存在则创建
+if [ ! -f "output_dir/test_tfqa_mc.json" ]; then
+  touch output_dir/test_tfqa_mc.json
+fi
+
+# 执行Python命令
+time python tfqa_mc_eval.py --model-name huggyllama/llama-7b --early-exit-layers 16,18,20,22,24,26,28,30,32 --data-path dataset --output-path output_dir/test_tfqa_mc.json --num-gpus 1
